7f9a0542af2b40a4c68b336b714f75e24b94e726
logan-james <loganjames448@gmail.com>
2024-04-25 21:11:25 -0700

    Using more than one input each input command stores different variable. If same variable is used to store more than one input each new value will replace the prevous one.

--- part01-08_name_and_address/src/name_and_address.py
+++ part01-08_name_and_address/src/name_and_address.py
@@ -1,1 +1,9 @@
 # Write your solution here
+fName = input("Given name: ")
+lName = input("Family name: ")
+addy = input("Street address: ")
+cityAndPost = input("Cit and postal code:")
+
+print(fName + " " + lName)
+print(addy)
+print(cityAndPost)
