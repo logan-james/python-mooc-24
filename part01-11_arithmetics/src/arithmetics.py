11541aa46db186f846104de88fadb8fffc5ce926
logan-james <loganjames448@gmail.com>
2024-09-27 16:49:36 -0700

    Using printf for arithmetics

--- part01-11_arithmetics/src/arithmetics.py
+++ part01-11_arithmetics/src/arithmetics.py
@@ -1,3 +1,11 @@
 # Write your solution here
 x = 27
 y = 15
+addition = (x + y)
+sub = x - y
+mult = x * y
+div = x / y
+print(f"{x} + {y} = {addition}")
+print(f"{x} - {y} = {sub}")
+print(f"{x} * {y} = {mult}")
+print(f"{x} / {y} = {div}")
