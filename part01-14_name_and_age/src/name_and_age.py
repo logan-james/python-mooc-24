2e4e5318e98bc7956f426267554946f5d18a4a0a
logan-james <loganjames448@gmail.com>
2024-10-02 19:12:48 -0700

    Using basic logic, variable and print f statements

--- part01-14_name_and_age/src/name_and_age.py
+++ part01-14_name_and_age/src/name_and_age.py
@@ -1,1 +1,5 @@
 # Write your solution here
+name = input("What is your name? ")
+birthday = int(input("What year were you born? "))
+age = 2021 - birthday
+print(f"Hi {name}, you will be {age} years old at the end of the year 2021")
