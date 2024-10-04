6232c9e239bec672c487c9324920878ca587b7d2
logan-james <loganjames448@gmail.com>
2024-10-04 10:44:22 -0700

    Basic logic

--- part01-17_sum_and_product/src/sum_and_product.py
+++ part01-17_sum_and_product/src/sum_and_product.py
@@ -1,1 +1,7 @@
 # Write your solution here
+n1 = int(input("Number 1: "))
+n2 = int(input("Number 2: "))
+sum = n1 + n2
+product = n1 * n2
+print(f"The sum of the numbers: {sum}")
+print(f"The product of the numbers: {product}")
