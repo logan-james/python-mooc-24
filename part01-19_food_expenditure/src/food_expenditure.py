1522e627fc5a9260e883ef6676d7d582854ed558
logan-james <loganjames448@gmail.com>
2024-10-07 16:19:37 -0700

    Using proper structure for arithmetic operations

--- part01-19_food_expenditure/src/food_expenditure.py
+++ part01-19_food_expenditure/src/food_expenditure.py
@@ -1,1 +1,13 @@
 # Write your solution here
+wk = int(input("How many times a week do you eat at the student cafeteria? "))
+p = float(input("The price of a typical student lunch? "))
+pg = float(input("How much money do you spend on groceries in a week? "))
+
+aw = (wk * p) + pg
+ad = ((wk * p) + pg) / 7
+
+print()
+print("Average food expenditure: ")
+print(f"Daily: {ad} euros")
+print(f"Weekly: {aw} euros")
+
