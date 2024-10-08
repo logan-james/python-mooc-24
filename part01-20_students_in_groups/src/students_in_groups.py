0dcb5c1d5b836a7766aae51ace131dac7d1b7d43
logan-james <loganjames448@gmail.com>
2024-10-07 20:46:32 -0700

    Using floor division // then conditional statement.

--- part01-20_students_in_groups/src/students_in_groups.py
+++ part01-20_students_in_groups/src/students_in_groups.py
@@ -1,1 +1,15 @@
 # Write your solution here
+
+# Ask the user for the number of students and the desired group size
+students = int(input("How many students on the course? "))
+group_size = int(input("Desired group size? "))
+
+# Calculate the number of groups
+num_groups = students // group_size
+
+# If there's a remainder, add one more group for the remaining students
+if students % group_size != 0:
+    num_groups += 1
+
+# Print the number of groups formed
+print(f"Number of groups formed: {num_groups}")
