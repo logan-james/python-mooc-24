f54a75c15ccf1154ae374e5cb15a4df082675389
logan-james <loganjames448@gmail.com>
2024-10-10 20:55:55 -0700

    Using else statment, fuck Jerry

--- part01-23_soup_or_no_soup/src/soup_or_no_soup.py
+++ part01-23_soup_or_no_soup/src/soup_or_no_soup.py
@@ -1,1 +1,9 @@
 # Write your solution here
+name = input("Please tell me your name: ")
+if name == "Jerry":
+  print("Next please!")
+else:
+  portions = int(input("How many portions of soup? "))
+  total = portions * 5.9
+  print(f"The total cost is {total}")
+  print("Next please!")
