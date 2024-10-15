8961f214e175674590f8b6a6c629b67cb1a32e58
logan-james <loganjames448@gmail.com>
2024-10-15 13:57:06 -0700

    Converting Fahrenheit to Celcius

--- part01-26_temperatures/src/temperatures.py
+++ part01-26_temperatures/src/temperatures.py
@@ -1,1 +1,8 @@
 # Write your solution here
+# Formula: celsius=(fahrenheit−32)∗5/9
+
+f = int(input("Please type in a temperature (F): "))
+c = (f - 32) * 5/9
+print(f"{f} degrees Fahrenheit equals {c} degrees Celsius")
+if c < 0:
+  print("Brr! It's cold in here!")
