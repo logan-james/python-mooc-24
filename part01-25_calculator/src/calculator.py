23b561cdc2bec51ccc4366c37209248375b99d10
logan-james <loganjames448@gmail.com>
2024-10-15 13:47:13 -0700

    creating simple calculator with three functions.

--- part01-25_calculator/src/calculator.py
+++ part01-25_calculator/src/calculator.py
@@ -1,1 +1,12 @@
 # Write your solution here
+a = int(input("Number 1: "))
+b = int(input("Number 2: "))
+operation = input("Operation: ")
+if operation == "add":
+  print(f" {a} + {b} = {a+b}")
+
+if operation == "multiply":
+  print(f"{a} * {b} = {a*b}")
+
+if operation == "subtract":
+  print(f"{a} - {b} = {a-b}")
