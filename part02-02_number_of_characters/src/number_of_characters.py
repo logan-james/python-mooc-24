1a0b491c1b209abc628a42311d96f9439c6ee300
logan-james <loganjames448@gmail.com>
2024-10-21 16:40:25 -0700

    Using 'len' function to find legnth of input

---
+++ part02-02_number_of_characters/src/number_of_characters.py
@@ -0,0 +1,11 @@
+# Write your solution here
+word = input("Please type in a word: ")
+
+length =  len(word)
+
+if length <= 1:
+  print("Thank you!")
+else:
+  print(f"There are {length} letters in the word {word}")
+  print("Thank you!")
+
