0e7c363e3fe557b9a515ae9272f61615a9ef5145
logan-james <loganjames448@gmail.com>
2024-11-26 20:23:46 -0800

    using while loop with conditional

---
+++ part02-18_repeat_password/src/repeat_password.py
@@ -0,0 +1,19 @@
+# Write your solution here
+# while True:  
+#   pw = input("Password: ")
+#   repeat_pw = input("Repeat Password: ")
+#   if pw == repeat_pw:
+#     print("User account created!")
+#     break
+#   else:
+#     print("They do not match!")
+
+# Write your solution here
+password = input("Password: ")
+while True:
+    repeat_password = input("Repeat password: ")
+    if repeat_password == password:
+        print("User account created!")
+        break
+    else:
+        print("They do not match!")
