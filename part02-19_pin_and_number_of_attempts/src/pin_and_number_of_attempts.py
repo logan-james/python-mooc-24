4ae422a6d59f3ca9f7c4e9d33f05ac909b7ff6fd
logan-james <loganjames448@gmail.com>
2024-12-01 17:20:11 -0800

    Only allowing 3 trys to PIN attempts.

---
+++ part02-19_pin_and_number_of_attempts/src/pin_and_number_of_attempts.py
@@ -0,0 +1,17 @@
+# Write your solution here
+
+attempts = 0
+
+while True:
+  pin = int(input("PIN: "))
+  
+  attempts += 1
+  if pin == 4321:
+    if attempts == 1:
+      print("Correct! It only took you one single attempt!")
+      break
+
+    print("Correct! It took you", attempts, "attempts")
+    break
+  else:
+    print("Wrong")
