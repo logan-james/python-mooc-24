0a113be241d994fc06eb482be43ef5b1485e1add
logan-james <loganjames448@gmail.com>
2024-12-13 15:40:09 -0800

    Updated with comments

--- part02-20_next_leap_year/src/next_leap_year.py
+++ part02-20_next_leap_year/src/next_leap_year.py
@@ -2,12 +2,12 @@
 
 
 start_year = int(input("Year: "))
-year = start_year + 1
-while True:
-    if year % 100 == 0:
+year = start_year + 1 #program starts checking for leap years starting after input
+while True: # loop continuously until leap year breaks out
+    if year % 100 == 0: #if year is div by 100 it must also be div by 400 to be leap yr
         if year % 400 == 0:
             break
-    elif year % 4 == 0:
+    elif year % 4 == 0: #if yr is div by 4 but not 100 it is leap
         break
     year += 1
 
